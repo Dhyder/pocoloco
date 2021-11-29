d75b9468147b197b81f824dd3a3b6a4783ac3a74
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-11-29 10:12:48 +0300

    defined url patterns for search functionality and location

--- galleria/urls.py
+++ galleria/urls.py
@@ -1,2 +1,9 @@
 from django.conf.urls import url
 from . import views
+
+
+urlpatterns=[
+    url('^$',views.gallery,name = 'gallery'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(\d+)',views.location_filter,name ='location')
+]
