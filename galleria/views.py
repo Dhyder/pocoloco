bc534ea5e53fbff7a31207fc3299cc0149672bd2
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-11-29 10:19:14 +0300

    created first view ==def gallery

--- galleria/views.py
+++ galleria/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 from .models import Image, Location, Category, Editor
+
+# Create your views here.
+def gallery(request):
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    return render(request, 'gallery.html', {"images":images, "locations":locations})
