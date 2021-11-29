dc8dddfade7b73b0ec35be335bd3aa5ee72d17a9
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-11-29 10:14:38 +0300

    imported static & settings and defined debug urlpattern for document root

--- galleria/urls.py
+++ galleria/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[
@@ -7,3 +9,6 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^location/(\d+)',views.location_filter,name ='location')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
