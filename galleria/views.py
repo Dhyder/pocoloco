a2b17f0d9423c2b1782c2191a4b7055b29e78e18
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-11-29 10:20:45 +0300

    created second view ==def search_results

--- galleria/views.py
+++ galleria/views.py
@@ -7,3 +7,19 @@ def gallery(request):
     images = Image.get_all_images()
     locations = Location.objects.all()
     return render(request, 'gallery.html', {"images":images, "locations":locations})
+
+
+def search_results(request):
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    if 'image' in request.GET and request.GET["image"]:
+        image_category = request.GET.get("image")
+        searched_images = Image.search_by_category(image_category)
+
+        message = f"{image_category}"
+
+        return render(request, 'search.html', {"message":message, "images": searched_images, 'categories': categories, "locations":locations})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message":message, "locations":locations})
